3c5ecc3b8956ec3f2c540515115d8e63a8320eb8
Michael Tarng <mtarng@highnoteplatform.com>
2021-12-09 23:38:17 -0800

    adding day templates

---
+++ day-template/part1.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+
+# file1 = open(os.path.join(os.path.dirname(__file__), 'testinput.txt'), 'r')
+file1 = open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r')
+lines = file1.readlines()
+
